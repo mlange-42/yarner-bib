fb19e5aab8c32daa788128b2942520779ab83033
Martin Lange <martin_lange_@gmx.net>
2021-02-28 16:58:43 +0100

    read bibliography file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use biblatex::Bibliography;
 use std::error::Error;
+use std::path::Path;
 use yarner_lib::Context;
 
 fn main() {
@@ -13,9 +15,16 @@ fn main() {
 
 fn run() -> Result<(), Box<dyn Error>> {
     let data = yarner_lib::parse_input()?;
-    let _config = &data.context.config;
+    let config = &data.context.config;
 
     check_version(&data.context);
+
+    let bib_file = config
+        .get("bibliography")
+        .and_then(|s| s.as_str())
+        .unwrap_or("bibliography.bib");
+
+    let _bibliography = load_bibliography(bib_file)?;
 
     yarner_lib::write_output(&data)?;
     Ok(())
@@ -31,4 +40,15 @@ fn check_version(context: &Context) {
             context.yarner_version
         )
     }
+}
+
+fn load_bibliography<P: AsRef<Path>>(file: P) -> Result<Bibliography, Box<dyn Error>> {
+    Bibliography::parse(&std::fs::read_to_string(&file).map_err(|err| {
+        format!(
+            "Can't read bibliography from file {} - {}",
+            file.as_ref().display(),
+            err.to_string()
+        )
+    })?)
+    .ok_or_else(|| format!("No valid bibliography in file {}", file.as_ref().display()).into())
 }
